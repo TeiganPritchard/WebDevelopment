4d60f7102afdd969715a41ea04932e0147a5670b
TeiganPritchard <teigan.pritchard@dcmail.ca>
2025-04-04 06:07:58 -0400

    finished javascript for assignment 4 part 2

--- Assignment-4-Part2/main.js
+++ Assignment-4-Part2/main.js
@@ -1,3 +1,8 @@
+//Name: Teigan Pritchard
+//Filename: main.js
+//Date: 2025/04/03
+//Description: Story name randomizer
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -5,14 +10,46 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 
 /* Declaring the alternative text for each image file */
 
+const alternativeText = {'pic1.jpg' : 'picture of eye',
+    'pic2.jpg' : 'picture of seashell close up',
+    'pic3.jpg' : 'picture of purple and white flowers',
+    'pic4.jpg' : 'picture of egyption art',
+    'pic5.jpg' : 'picture of butterfly on leaf'
+  }
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for ( image of images) {
+    addImage = document.createElement('img');
+    addImage.setAttribute('src', `images/${image}`);
+    addImage.setAttribute('alt', alternativeText[image]);
+    thumbBar.appendChild(addImage);
+
+
+    addImage.addEventListener('click', clickedImage => {
+      displayedImage.src = clickedImage.target.src;
+      displayedImage.alt = clickedImage.target.alt;
+    });
+  }
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+
+    currentSelection = btn.getAttribute("class");
+
+    if (currentSelection === "dark") {
+      btn.setAttribute("class","light");
+      btn.textContent = "Lighten";
+      overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+
+    } else {
+      btn.setAttribute("class","dark");
+      btn.textContent = "Darken";
+      overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    }
+  });
+  
